462554c2d712adf118bde3e53a637207f80d9eb2
Patrik Johnson <patrik@zebros.fi>
2014-01-10 23:41:15 +0200

    Basic express routes with corresponding jade templates

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
@@ -14,16 +12,38 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
-app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(app.router);
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.use(function(req, res, next) {
+  res.status(404);
+  if (req.accepts('html')) {
+    res.render('404', { title: 'Express Dynamic URLs - no such page' });
+    return;
+  }
+  if (req.accepts('json')) {
+    res.send({ error: 'Not found' });
+    return;
+  }
+  res.type('txt').send('Not found');
+});
+
+app.get('/(rooms)?/?', function(req, res) {
+  res.render('index', { title: 'Express Dynamic URLs' });
+});
+
+app.get('/rooms/:id', function(req, res) {
+  var roomId = req.params.id;
+  if (true) {
+    res.render('room', { title: 'Room id: ' + roomId, name: roomId });
+  }
+  res.render('no_room', { title: 'Couldn\'t find that room', name: roomId });
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
